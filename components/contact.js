da5eef94774ddbb40184de6cbaf4ae848f923e41
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2020-09-09 18:29:13 -0700

    fixed button

--- components/contact.js
+++ components/contact.js
@@ -32,15 +32,15 @@ const Contact = () => {
                 </div>
               <div className="contact__inputContainer">
                 <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                <input value={email} type="text" size="35" id="contactEmail" name="contactEmail" onChange={e=> setEmail(e.target.value)}/>
+                <input value={email} type="text" size="35" name="contactEmail" onChange={e=> setEmail(e.target.value)}/>
               </div>
               <div className="contact__inputContainer">
                 <label htmlFor="contactSubject">Subject </label>
-                <input value={subject} type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={e => setSubject(e.target.value)}/>
+                <input value={subject} type="text" defaultValue="" size="35" name="contactSubject" onChange={e => setSubject(e.target.value)}/>
               </div>
               <div className="contact__inputContainer">
                 <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                <textarea value={message} onChange={e => setMessage(e.target.value)} cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
+                <textarea value={message} onChange={e => setMessage(e.target.value)} name="contactMessage"></textarea>
               </div>
               <div className="contact__button">
                 <Button type='submit' onClick={handleClick}>Submit</Button>
--- components/projects2.js
+++ components/projects2.js
@@ -7,18 +7,38 @@ function Projects2() {
         <div className="project2__body">
             <h1>Check Out Some of My <b>Projects</b>.</h1>
             <div className="project2__content">
-            <a href="https://serene-albattani-33a131.netlify.app">
-              <img src="./img/stay_inside.png" alt="" />
-            </a> 
-            <a href="https://seir-auth.herokuapp.com/">
-              <img src="./img/my_anime.png" alt="" />
-            </a> 
-            <a href="https://whats-app-clone-c54dd.web.app/">
-              <img src="./img/whatsapp.png" alt="" />
-            </a> 
-            <a >
-              <img src="./img/breakeout.png" alt="" />
-            </a> 
+              <a href="https://serene-albattani-33a131.netlify.app">
+                <p>
+                  <h3>Stay Inside</h3>
+                  <p>Stay-inside is a social media web app made to encourage people to have an online gathering events during this pandemic.</p>
+                  <p>It was created using JavaScript, Express, React, MongoDB and Node.</p>
+                </p>
+                <img src="./img/stay_inside.png" alt="" />
+              </a> 
+              <a href="https://seir-auth.herokuapp.com/">
+                <p>
+                  <h3>MyAnime</h3>
+                  <p>MyAnime is a full stack app allowing users to create a list of their favorite anime and manga.</p>
+                  <p>It was created using Javasript, PostgreSQL, Sequelize and Jikan API.</p>
+                </p>
+                <img src="./img/my_anime.png" alt="" />
+              </a> 
+              <a href="https://whats-app-clone-c54dd.web.app/">
+                <p>
+                  <h3>What's App Clone</h3>
+                  <p>Users can create a room and chat with other users.</p>
+                  <p>It was created using JavaScript, HTML, CSS, Materia-UI ReactJs, React Context API, Firebase And Google Authentication</p>
+                </p>
+                <img src="./img/whatsapp.png" alt="" />
+              </a> 
+              <a >
+                <p>
+                  <h3>Breakout</h3>
+                  <p>Users can control the paddle with the mouse to bounce off the ball and destroy all the bricks.</p>
+                  <p>It was created using JavaScript, HTML-Canvas</p>
+                </p>
+                <img src="./img/breakeout.png" alt="" />
+              </a> 
             </div>
           </div>
       </div>
